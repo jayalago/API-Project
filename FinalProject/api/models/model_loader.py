d6d11d74db5868a6f47a341f10893a70909f994a
Jacky <Jayalago@charlotte.edu>
2025-11-05 16:49:58 -0500

    updated and added the new models

--- FinalProject/api/models/model_loader.py
+++ FinalProject/api/models/model_loader.py
@@ -1,11 +1,16 @@
-from . import orders, order_details, recipes, sandwiches, resources
+from . import orders, order_details, recipes, sandwiches, resources, customer, payment, promotion, rating
 
 from ..dependencies.database import engine
 
 
 def index():
-    orders.Base.metadata.create_all(engine)
+
+    customer.Base.metadata.create_all(engine)
     order_details.Base.metadata.create_all(engine)
+    orders.Base.metadata.create_all(engine)
+    payment.Base.metadata.create_all(engine)
+    promotion.Base.metadata.create_all(engine)
+    rating.Base.metadata.create_all(engine)
     recipes.Base.metadata.create_all(engine)
-    sandwiches.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
+    sandwiches.Base.metadata.create_all(engine)
