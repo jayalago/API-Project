7ec90a4a7f59a12ef3c9383360f4fc1893bb9d50
thelemonsqueezed <isabelle.lemmons@gmail.com>
2025-11-17 14:56:21 -0500

    feat: Deleted recipe.py schema and made progress on MENU section of main.py

--- FinalProject/api/main.py
+++ FinalProject/api/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
+from .dependencies.database import engine, SessionLocal, get_db #need to figure out how to import these from the database.py in "dependencies"
 from .routers import index as indexRoute
 from .models import model_loader
 from .dependencies.config import conf
@@ -9,9 +10,12 @@ from pydantic import BaseModel
 from typing import Annotated
 from sqlalchemy.orm import Session
 from sqlalchemy import func
-#import schemas file somehow
-# all of the new imports are from python project 2
+from .schemas import *
+from .models import *
+
+'''all of the new imports are from python project 2
 # that's what i'm using as a reference
+A lot fo these may end up not even being necessary idk'''
 
 
 app = FastAPI()
@@ -28,21 +32,23 @@ app.add_middleware(
 
 model_loader.index()
 indexRoute.load_routes(app)
-
+models.Base.metadata.create_all(bind=engine)
 db_dependency = Annotated[Session, Depends(get_db)] #needs to be fixed, unsure how
 
 if __name__ == "__main__":
     uvicorn.run(app, host=conf.app_host, port=conf.app_port)
 
-
+# I think once the Menu schema is completed, this will work better
 # MENU
 @app.get("/menu/", status_code=status.HTTP_200_OK)
 async def get_menu(db: db_dependency):
-    # return the recipes database
+   return db.query(models.Menu).all()
 
 @app.post("/menu/", status_code = status.HTTP_201_CREATED)
-async def add_menu_item(): #insert stuff inside parenthesis
-    #exactly as it sounds, add a recipe to the recipe database
+async def add_menu_item(menu: MenuBase, db: db_dependency):
+    db_menu = models.Menu(**todo.model_dump())
+    db.add(db_menu)
+    db.commit()
     return {"detail": "Item added successfully."}
 
 @app.put("/menu/{menu_id}", response_model = MenuBase, status_code=status.HTTP_200_OK )
@@ -54,3 +60,9 @@ async def update_menu_item(menu_id: int): # more goes in the parenthesis
 async def delete_menu_item(menu_id: int): #more goes inside parenthesis
     #stuff goes here
     return {"detail": "Item deleted successfully."}
+
+# Orders
+
+# Customers
+
+#
