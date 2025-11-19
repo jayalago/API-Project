f6ee2182100eb94425db5ea134cc229d3a7a7da9
Alex Khamphoumy <akhampho@charlotte.edu>
2025-11-19 15:13:56 -0500

    feat: added in code for menu schemas.

--- FinalProject/api/schemas/menu.py
+++ FinalProject/api/schemas/menu.py
@@ -3,16 +3,20 @@ from typing import Optional
 from pydantic import BaseModel
 
 class MenuBase(BaseModel):
-    pass
+    sandwich_id: int
+    resource_id: int
+    price: float
 
 class MenuCreate(MenuBase):
-    pass
+    sandwich_id: Optional[int] = None
+    resource_id: Optional[int] = None
+    price: Optional[float] = None
 
 class MenuUpdate(BaseModel):
     pass
 
 class Menu(MenuBase):
-    pass
+    id: int
 
     class ConfigDict:
         from_attributes = True
